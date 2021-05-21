78a97e9134c2a484b04586041454fdcd9d32c284
n3n7i <25413904+n3n7i@users.noreply.github.com>
2021-05-21 12:33:44 +0800

    Create libFuse.h

---
+++ src/initial/libFuse.h
@@ -0,0 +1,89 @@
+
+
+#include "wirePlus.h"
+
+#define libfuse_MAXvar 6
+#define libfuse_MAXrD libfuse_MAXvar*2
+
+class IMU{
+
+  public:
+    IMU(byte addr){ xAddr = addr;}
+    byte xAddr;
+    
+    //byte readData[libfuse_MAXrD];
+    short xreadData[libfuse_MAXvar];
+
+    void setup_IMU();
+    void fifo_Data();
+    //void fifo_Data();
+    };
+
+void IMU::setup_IMU(){
+  regWrite(xAddr, 0x6B, 0);
+  delay(100);
+  regWrite(xAddr, 0x1C, 0x03<<3); // xl: +-16g // 2048lsb/g
+  regWrite(xAddr, 0x1B, 0x03<<3); // gyro: +-2000deg // 16.4lsb/deg
+  regWrite(xAddr, 0x1A, 0x06);    // lowpass: 20Hz (0x04)?
+  regWrite(xAddr, 0x19, 0x40);    // sampdiv 1000/32 (1khz/32)
+  
+  regWrite(xAddr, 0x23, 0x78); // xyzg fifo
+  //regWrite(xAddr, 0x23, 0x00); // xyzg fifo disable
+  
+  regWrite(xAddr, 0x6A, 0x04); // fifo reset
+  regWrite(xAddr, 0x6A, 0x40); // fifo en
+  
+  delay(25);
+  regWrite(xAddr, 0x6B, 0x00); //def clock
+
+  regWrite(xAddr, 0x38, 0x01); //data ready flag enable
+  
+  }
+
+
+void IMU::fifo_Data(){
+
+  /*regRequest(xAddr, 0x72, 2);
+  int ififo = Wire.read()<<8 | Wire.read();
+  Wire.endTransmission(true);
+
+  if(ififo>12){
+    for(int i=0; i<ififo; i++){
+      Wire.read();
+      }
+    }
+*/
+  regRequest(xAddr, 0x72, 2);
+  int ififo = Wire.read()<<8 | Wire.read();
+  Wire.endTransmission(true);
+
+  //Serial.print(regRead(xAddr, 0x3A) & 0x01);
+  //Serial.print("dataReady ");
+
+  if(ififo >= 12){
+    //Serial.print(ififo);
+    //Serial.print("F ");
+
+    //Serial.print(regRead(xAddr, 0x3A));
+    //Serial.print("dataReady ");
+
+    regRequest(xAddr, 0x74, 12);
+
+    int j=0;
+    byte b1, b2;
+    for(int i=0; i<12; i+=2){
+      //readData[i] = Wire.read();
+      //readData[i+1] = Wire.read();
+      b1 = Wire.read(); b2 = Wire.read();
+      //xreadData[j] = readData[i]<<8|readData[i+1];
+      xreadData[j] = b1<<8|b2;
+      j++;
+      //Serial.print(readData[i], HEX);
+      //Serial.print(" ");
+      }
+
+    //Serial.println("OK");
+    
+    Wire.endTransmission(true);
+    }
+  }
